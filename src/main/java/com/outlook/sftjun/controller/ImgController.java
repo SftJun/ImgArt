c09a8732012f17a9fd3368393fbb1a8c0627464f
YinJun <sftjun@outlook.com>
2017-01-10 00:38:29 +0800

    save img to db and add session userid

--- src/main/java/com/outlook/sftjun/controller/ImgController.java
+++ src/main/java/com/outlook/sftjun/controller/ImgController.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
@@ -15,11 +16,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Import;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.outlook.sftjun.config.PropertyPlaceholderConfig;
 import com.outlook.sftjun.domain.Img;
@@ -30,9 +33,9 @@ import com.outlook.sftjun.tools.PicTypes;
 @Controller
 @RequestMapping(value = "/img")
 @Import(PropertyPlaceholderConfig.class)
-@SessionAttributes("user")
+@SessionAttributes("userId")
 public class ImgController extends BaseController {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(ImgController.class);
 
 	@Autowired
@@ -42,8 +45,14 @@ public class ImgController extends BaseController {
 	private String fileUploadDirectory;
 
 	@RequestMapping(value = "upload", method = RequestMethod.GET)
-	public String upLoad() {
+	public String upLoad(HttpSession httpSession) {
 		log.info("图片上传的Get请求");
+		Object sessionObj = httpSession.getAttribute("userId");
+		if (sessionObj == null || sessionObj.toString().equals("")) {
+			log.info("没有\"userId\"对应的Session");
+		} else {
+			log.info(httpSession.getAttribute("userId").toString());
+		}
 		return "/imgs/upload";
 	}
 
@@ -59,11 +68,11 @@ public class ImgController extends BaseController {
 			HttpServletResponse response) {
 		log.info("图片上传的POST请求");
 		//
-		Map<String,String[]> parameterMap = request.getParameterMap();
-		String location = new StringBuilder(parameterMap.get("location")[0]).toString();
-		log.info("地址是:"+location);
-		String shortInfo = new StringBuilder(parameterMap.get("shortInfo")[0]).toString();
-		log.info("简介是:"+shortInfo);
+		Map<String, String[]> parameterMap = request.getParameterMap();
+		String location = parameterMap.get("location")[0];
+		log.info("地址是:" + location);
+		String shortInfo = parameterMap.get("shortInfo")[0];
+		log.info("简介是:" + shortInfo);
 		//
 		for (CommonsMultipartFile file : files) {
 			Img img = new Img();
@@ -76,7 +85,7 @@ public class ImgController extends BaseController {
 			PicTypes picTypes = new PicTypes();
 			if (picTypes.getPIC_TYPES().contains(type)) {
 				// 自定义的文件名称
-				String trueFileName = UUID.randomUUID().toString() +"."+ type;
+				String trueFileName = UUID.randomUUID().toString() + "." + type;
 				// 设置存放图片文件的路径
 				path = fileUploadDirectory + trueFileName;
 				img.setImgName(fileName);
@@ -84,7 +93,7 @@ public class ImgController extends BaseController {
 				img.setContentType(type);
 				img.setLocation(location);
 				img.setShortInfo(shortInfo);
-				img.setUser(null);//TODO
+				img.setUser(null);// TODO
 				log.info("存放图片文件的路径:" + path);
 				try {
 					// 转存文件到指定的路径
@@ -95,7 +104,7 @@ public class ImgController extends BaseController {
 				} catch (IOException e) {
 					log.error(e.toString());
 				}
-				log.info("文件成功上传到"+path);
+				log.info("文件成功上传到" + path);
 			} else {
 				log.info("不是我们想要的文件类型,请按要求重新上传");
 			}
