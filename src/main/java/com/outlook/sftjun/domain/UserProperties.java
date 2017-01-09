25ede0fceeb5c4283eb69d206a159d8f7650af23
YinJun <sftjun@outlook.com>
2017-01-10 00:34:46 +0800

    User　class's propertes string

---
+++ src/main/java/com/outlook/sftjun/domain/UserProperties.java
@@ -0,0 +1,9 @@
+package com.outlook.sftjun.domain;
+
+public class UserProperties {
+	public static final String USERNAME = "USERNAME";
+	public static final String PASSWORD = "PASSWORD";
+	public static final String AGE = "AGE";
+	public static final String GENDER = "GENDER";
+	public static final String TEL = "TEL";
+}
